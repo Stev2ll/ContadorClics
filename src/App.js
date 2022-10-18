c31c625ec37885fe30637744a1cf1497d629000e
BryanLlulluna <llullunab@gmail.com>
2022-10-18 02:27:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import MiLogoStev from './img/MI-Logo.gif';
+import {useState} from 'react';
 
 function App() {
+
+  //Aqui se van a colocar las funciones
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () =>{
+    setNumClics(numClics+1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Mi-logo-contenedor'>
+        <img 
+          className='MiLogo'
+          src= {MiLogoStev}
+          alt='Logo Stev2ll'
+        />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador
+          numClics={numClics}
+        />
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic}
+        />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
+      <footer>
+        <h3>creado por Stev2ll</h3>
+      </footer>
     </div>
   );
 }
